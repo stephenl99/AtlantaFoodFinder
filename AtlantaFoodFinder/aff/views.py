5cbbd05f2cf93250df1ff9c107724680d7d2d65b
clinder2 <clinder9@gatech.edu>
2024-05-31 12:21:26 -0400

    Added base template to display Google map.  Modified index method to display new template.

--- AtlantaFoodFinder/aff/views.py
+++ AtlantaFoodFinder/aff/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import loader
 
 
 def index(request):
-    return HttpResponse("Are you hungry Atlanta?")
+    #template = loader.get_template("html.html")
+    return render(request, "html.html")
+    #return HttpResponse(template.render({}, request))
+    #return HttpResponse("Are you hungry Atlanta?")
