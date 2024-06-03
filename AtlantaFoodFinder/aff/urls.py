e56b94f154cfa529cecc1d94cf95330a94a33542
clinder2 <clinder9@gatech.edu>
2024-06-03 12:45:19 -0400

    Created index files with js map object to manipulate

--- AtlantaFoodFinder/aff/urls.py
+++ AtlantaFoodFinder/aff/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from .views import *
 
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("index", MapView.as_view(), name='map_view'),
 ]
--- AtlantaFoodFinder/aff/views.py
+++ AtlantaFoodFinder/aff/views.py
@@ -1,10 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader
+from django.views import View
 
 
 def index(request):
     #template = loader.get_template("html.html")
-    return render(request, "html.html")
+    return render(request, "aff/html.html")
     #return HttpResponse(template.render({}, request))
     #return HttpResponse("Are you hungry Atlanta?")
+
+class MapView(View):
+    template_name = "aff/index.html"
+    def get(self,request):
+        context = {
+        }
+
+        return render(request, self.template_name, context)
