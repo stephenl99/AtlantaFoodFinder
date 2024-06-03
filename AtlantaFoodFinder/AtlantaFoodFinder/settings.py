e6be3109e87d7cb387a860c85e3e0938e0b5d878
Charlie McHenry <chmch@Charlies-MacBook-Pro.local>
2024-06-03 11:47:28 -0400

    added basic geodjango functionality along with admin site

--- AtlantaFoodFinder/AtlantaFoodFinder/settings.py
+++ AtlantaFoodFinder/AtlantaFoodFinder/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.gis",
+    "aff",
 ]
 
 MIDDLEWARE = [
@@ -76,8 +78,8 @@ WSGI_APPLICATION = "AtlantaFoodFinder.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.contrib.gis.db.backends.postgis",
+        "NAME": "postgres",
     }
 }
 
--- AtlantaFoodFinder/aff/admin.py
+++ AtlantaFoodFinder/aff/admin.py
@@ -1,3 +1,6 @@
-from django.contrib import admin
+from django.contrib.gis import admin
+from .models import WorldBorder
 
 # Register your models here.
+
+admin.site.register(WorldBorder, admin.ModelAdmin)
