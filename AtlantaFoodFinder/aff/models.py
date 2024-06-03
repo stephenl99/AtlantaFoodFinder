690b5668cc27fee1123aa018a59d59ceb0232c61
clinder2 <clinder9@gatech.edu>
2024-06-02 20:11:10 -0400

    Added base template to display Google map.  Modified index method to display new template.

--- AtlantaFoodFinder/aff/models.py
+++ AtlantaFoodFinder/aff/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+import googlemaps
+from googleplaces import GooglePlaces
 
 # Create your models here.
+
+class Resturant(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    zipcode = models.CharField(max_length=100)
+    #addressName = str(address) + ", " + str(zipcode) + ", Atlanta, Georgia"
+    phone = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    cuisine = models.CharField(max_length=100)
+    def getGeoCode(self):
+        gmaps = googlemaps.Client(key='AIzaSyAFti1ky6h6R9RLNpRgRHAOLrAR5XXMU4A')
+        #gmaps.find_place("Atlanta", 'textquery')
+        result = gmaps.geocode(str(self.address) + ", " + str(self.zipcode) + ", Atlanta, Georgia")[0]
+        gmaps.find_place()
+        return result
+
+    #gp = GooglePlaces("AIzaSyAFti1ky6h6R9RLNpRgRHAOLrAR5XXMU4A")
+    #q = gp.nearby_search(location='Atlanta',keyword='Resturant',radius=500)
