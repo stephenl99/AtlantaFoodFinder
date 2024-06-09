7958fc61897b4f1cf31f3782b15fda6d493518b5
clinder2 <clinder9@gatech.edu>
2024-06-09 13:44:27 -0400

    Create name database, added search functionality

---
+++ AtlantaFoodFinder/aff/static/aff/test.js
@@ -0,0 +1,10 @@
+//async function getRestaurant() {
+  //var input = document.getElementById('restaurant');
+  //filter = input.value.toUpperCase();
+  //alert("first");
+  //input.addEventListener("dblclick", function(event) {
+    //alert(filter);
+  //});
+//}
+//alert("a1");
+//getRestaurant();
