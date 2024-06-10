298524ac9fa81326fcdbd54c0cec9ce8390dbd93
Charlie McHenry <chmch@lawn-10-91-9-176.lawn.gatech.edu>
2024-06-10 12:29:38 -0400

    added user login and registration functionality

--- AtlantaFoodFinder/aff/views.py
+++ AtlantaFoodFinder/aff/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.template import loader
+from django.contrib.auth import authenticate, login, logout
+from AtlantaFoodFinder.forms import SignupForm, LoginForm
 
 
 def index(request):
@@ -8,3 +10,37 @@ def index(request):
     return render(request, "html.html")
     #return HttpResponse(template.render({}, request))
     #return HttpResponse("Are you hungry Atlanta?")
+
+
+# signup page
+def user_signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = SignupForm()
+    return render(request, 'signup.html', {'form': form})
+
+
+# login page
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user:
+                login(request, user)
+                return redirect('index')
+    else:
+        form = LoginForm()
+    return render(request, 'login.html', {'form': form})
+
+
+# logout page
+def user_logout(request):
+    logout(request)
+    return redirect('login')
