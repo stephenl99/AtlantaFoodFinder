f1a4b8f73f3ba38ebc8fa649fe53694d3964b10f
slinder8 <sjtlinder@icloud.com>
2024-07-19 13:18:01 -0400

    updated urls again

--- AtlantaFoodFinder/aff/urls.py
+++ AtlantaFoodFinder/aff/urls.py
@@ -4,7 +4,8 @@ from .views import *
 from . import views
 
 urlpatterns = [
-    path("https://atlantafoodfinder-a281298e3c3f.herokuapp.com/", views.index, name="index"),
+
+    path('', views.index, name="index"),
     path("index", views.MapView, name='map_view'),
     path('processMapView/', views.processMapView, name='processMapView'),
     path('removeMapView/', views.removeMapView, name='removeMapView'),
